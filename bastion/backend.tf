36d317c6b0e79a91ff51351b52195939dac1c0dd
Khessie Evans <khessieevans@yahoo.com>
2023-06-11 18:03:38 +0100

    bastion folder clean up

--- bastion/backend.tf
+++ bastion/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
-    key    = "jenkins-server/terraform.tfstate"
+    bucket = "group7-bastion"
+    region = "eu-west-2"
+    key    = "bastion/terraform.tfstate"
   }
 }
--- bastion/variable.tf
+++ bastion/variable.tf
@@ -1,9 +1,11 @@
 variable "vpc_cidr_block" {
   default = "10.0.0.0/16"
 }
+
 variable "subnet_cidr_block" {
   default = "10.0.10.0/24"
 }
+
 variable "avail_zone" {
   default = "eu-west-2a"
 }
